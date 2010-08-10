64f24a36dcb49a2725fb85dd71789017a4aacf3d
Unnikrishnan KP <unni.tallman@gmail.com>
2010-08-10 16:17:26 +0530

    minor hiccup caused to respond_to? by the use of "method_missing" FIXED.

--- lib/handler.rb
+++ lib/handler.rb
@@ -1,5 +1,6 @@
 module ActsAsAuthorizable
   module Handler
+    CAN_METHOD  = /^can_(.*)/
     class_eval do
       def handle_can_methods(feature_name,args)
         object   = args.first
@@ -8,18 +9,22 @@ module ActsAsAuthorizable
       end  
 
       def method_missing(method_name,*args)
-        r  = /^can_(.*)/
         r1 = /^can_/
         method_name = method_name.to_s
         
-        if method_name =~ r 
+        if method_name =~ CAN_METHOD 
           method_name.chop! if method_name[-1].chr == "?"
            handle_can_methods(method_name.gsub(r1,""),args)
         else 
            super(method_name.to_sym,*args)
         end  
-        
       end  
+      
+      def respond_to?(method)
+        return true if method.to_s =~ CAN_METHOD
+        super
+      end
+      
     end
   end
 end
